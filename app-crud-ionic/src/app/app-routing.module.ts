8e948012c2e70832be20cbc316819928eec75193
Erick Souza <esmerick@gmail.com>
2020-07-03 16:01:27 -0300

    Start from page home

--- app-crud-ionic/src/app/app-routing.module.ts
+++ app-crud-ionic/src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./index/index.module').then(m => m.IndexPageModule)
-  },
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./index/index.module').then(m => m.IndexPageModule)
+  // },
   {
     path: '',
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
